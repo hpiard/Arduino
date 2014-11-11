2ae79a2963701a8549bd04e92ec402dfaaf8b343
hpiard <heinrich@piard.de>
2014-11-11 17:47:56 -0500

    fixed formula for distance calculation (sensor sharp 2y0a21 F 3Z)

--- Robot/read_distance_sensor.py
+++ Robot/read_distance_sensor.py
@@ -21,18 +21,16 @@ def front_distance():
     while True:
         board.analog[ANALOG_0].enable_reporting()
         analog_value = board.analog[ANALOG_0].read()
-        #if analog_value == None:
-        #    analog_value = 0
-        #else:
         print "\n"
         print "Actual reading: " + str(analog_value)
         if analog_value == None or analog_value == 0 or analog_value == 0.0:
             analog_value = 1
-        print "Calculated Reading: " + str(analog_value * 1024)
-        distance = (3027.4 / analog_value)pow(1.2134)
+        #print "Calculated Reading: " + str(analog_value * 1024)
+        x = (3027.4 / (analog_value*1024))
+        distance = pow(x, 1.2134)
         print "============================================"
-        print "Front distance is: " + str(analog_value)
-        print "Calculated Distance:" + str(distance) + " cm"
+        #print "Front distance is: " + str(analog_value)
+        print "Calculated Front Distance:" + str(distance) + " cm"
         print "============================================"
         sleep(2)
 
@@ -43,21 +41,19 @@ def rear_distance():
     while True:
         board.analog[ANALOG_1].enable_reporting()
         analog_value = board.analog[ANALOG_1].read()
-        #if analog_value == None:
-        #    analog_value = 0
-        #else:
         print "\n"
         print "Actual reading: " + str(analog_value)
         if analog_value == None or analog_value == 0 or analog_value == 0.0:
             analog_value = 1
-        print "Calculated Reading: " + str(analog_value * 1024)
-        distance = (3027.4 / analog_value)pow(1.2134)
+        #print "Calculated Reading: " + str(analog_value * 1024)
+        y = (3027.4 / (analog_value*1024))
+        distance = pow(y, 1.2134)
         print "============================================"
-        print "Rear distance is: " + str(analog_value)
-        print "Calculated Distance:" + str(distance) + " cm"
+        #print "Rear distance is: " + str(analog_value)
+        print "Calculated Rear Distance:" + str(distance) + " cm"
         print "============================================"
         sleep(2)
 
 if __main__ == "__main__":
-    #front_distance()
+    front_distance()
     rear_distance()
