25de4da273da55bc89d1576ff9ce714303d77741
hpiard <heinrich@piard.de>
2014-11-16 22:59:28 -0500

    adding view more files and additional code

--- Robot/Driving2.py
+++ Robot/Driving2.py
@@ -4,7 +4,7 @@ __init__ = '__init__'
 import pyfirmata
 from pyfirmata import util
 from time import sleep
-
+from threading import Thread
 
 ANALOG_0 = 0
 ANALOG_1 = 1
@@ -57,58 +57,69 @@ def stop_car(on_off=0):
 def front_distance():
     it = util.Iterator(board)
     it.start()
-    board.analog[ANALOG_0].enable_reporting()
-    analog_value = board.analog[ANALOG_0].read()
-    #print "\n"
-    #print "Actual reading: " + str(analog_value)
-    if analog_value == None or analog_value == 0 or analog_value == 0.0:
-        analog_value = 1
-        #print "Calculated Reading: " + str(analog_value * 1024)
-        x = (3027.4 / (analog_value*1024))
-        distance = pow(x, 1.2134)
-        #return pow(x, 1.2134)
-        #print "============================================"
-        #print "Front distance is: " + str(analog_value)
-        #print "Calculated Front Distance:" + str(distance) + " cm"
-        #print "============================================"
-        #print str(distance)
-        return distance
+    while True:
+        board.analog[ANALOG_0].enable_reporting()
+        analog_value = board.analog[ANALOG_0].read()
+        #print "\n"
+        #print "Actual reading: " + str(analog_value)
+        if analog_value == None or analog_value == 0 or analog_value == 0.0:
+            analog_value = 1
+            #print "Calculated Reading: " + str(analog_value * 1024)
+            x = (3027.4 / (analog_value*1024))
+            distance = pow(x, 1.2134)
+            #return pow(x, 1.2134)
+            #print "============================================"
+            #print "Front distance is: " + str(analog_value)
+            #print "Calculated Front Distance:" + str(distance) + " cm"
+            #print "============================================"
+            print distance
+            #return distance
+            sleep(1)
 
 
 def rear_distance():
     it = util.Iterator(board)
     it.start()
-    board.analog[ANALOG_1].enable_reporting()
-    analog_value = board.analog[ANALOG_1].read()
-    #print "\n"
-    #print "Actual reading: " + str(analog_value)
-    if analog_value == None or analog_value == 0 or analog_value == 0.0:
-        analog_value = 1
-        #print "Calculated Reading: " + str(analog_value * 1024)
-        y = (3027.4 / (analog_value*1024))
-        distance = pow(y, 1.2134)
-        #return pow(y, 1.2134)
-        #print "============================================"
-        #print "Rear distance is: " + str(analog_value)
-        #print "Calculated Rear Distance:" + str(distance) + " cm"
-        #print "============================================"
-        #print str(distance)
-        return distance
+    while True:
+        board.analog[ANALOG_1].enable_reporting()
+        analog_value = board.analog[ANALOG_1].read()
+        #print "\n"
+        #print "Actual reading: " + str(analog_value)
+        if analog_value == None or analog_value == 0 or analog_value == 0.0:
+            analog_value = 1
+            #print "Calculated Reading: " + str(analog_value * 1024)
+            y = (3027.4 / (analog_value*1024))
+            distance = pow(y, 1.2134)
+            #return pow(y, 1.2134)
+            #print "============================================"
+            #print "Rear distance is: " + str(analog_value)
+            #print "Calculated Rear Distance:" + str(distance) + " cm"
+            #print "============================================"
+            print distance
+            #return distance
+            sleep(1)
 
 
 def driving():
+    Thread(target=front_distance).start()
+    Thread(target=rear_distance).start()
+
+    '''
     while True:
         front = front_distance()
+        print front
         back = rear_distance()
+        print back
         if front >= 3.7259080757:
             left_forward(1)
             right_forward(1)
+            sleep(1)
         else:
             stop_car()
-            print front
-            print back
+            print str(front)
+            print str(back)
             sleep(1)
-
+'''
 
 if __init__ == '__init__':
     driving()
